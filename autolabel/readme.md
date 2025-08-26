92ae4a4c067d4dcfb8cde2f60e9f101f4e233b8a
Stas Ukolov <ukoloff@gmail.com>
2025-08-26 14:59:05 +0500

    Use .env

--- autolabel/readme.md
+++ autolabel/readme.md
@@ -3,5 +3,10 @@
 Расстановка меток на узлы Swarm
 в зависимости от работающих на них сервисов
 
+# .env
+```
+DOCKER_HOST=ssh://root@swarm01.ekb.ru
+```
+
 ## See also
 [Original idea](davideshay/)
--- autolabel/src/index.js
+++ autolabel/src/index.js
@@ -1,8 +1,5 @@
 import Docker from 'dockerode'
 
-console.log('Hello, world!')
-
-process.env.DOCKER_HOST = 'ssh://root@swarm01.ekb.ru'
 let d = new Docker()
 let i = await d.info()
 console.log(i)
