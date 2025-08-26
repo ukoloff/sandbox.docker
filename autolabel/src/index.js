02b088d57a727c3d603d494adf45900da52fc946
Stas Ukolov <ukoloff@gmail.com>
2025-08-26 15:30:33 +0500

    Read data

--- autolabel/src/index.js
+++ autolabel/src/index.js
@@ -3,3 +3,17 @@ import Docker from 'dockerode'
 let d = new Docker()
 let i = await d.info()
 console.log(i)
+
+if (!i.Swarm.Nodes) {
+  console.error('Swarm not found!')
+  process.exit(1)
+}
+
+await gather(d)
+
+async function gather(docker) {
+  let n = await docker.listNodes()
+  let s = docker.listServices()
+  let t = docker.listTasks()
+  console.log('Nodes: ', n.length)
+}
