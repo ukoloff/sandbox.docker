63f289d8368239e839a489c13143dda9a469e6f0
Stas Ukolov <ukoloff@gmail.com>
2025-08-27 09:42:24 +0500

    Build labels table

--- autolabel/src/index.js
+++ autolabel/src/index.js
@@ -1,27 +1,54 @@
 import Docker from 'dockerode'
 
-let d = new Docker()
-let i = await d.info()
-console.log(i)
+let docker = new Docker()
+let i = await docker.info()
 
 if (!i.Swarm.Nodes) {
   console.error('Swarm not found!')
   process.exit(1)
 }
 
-await gather(d)
+await gather()
 
-async function gather(docker) {
-  let n = await docker.listNodes()
-  let s = docker.listServices()
-  let t = docker.listTasks()
-  console.log('Nodes: ', n.length)
-  let n0 = docker.getNode(n[0].ID)
-  let i = await n0.inspect()
-  let upd = {
-    version: i.Version.Index,
-    ...i.Spec,
-    Labels: { a: "AAA", b: "BBB" },
+async function gather() {
+  let t = await buildTable()
+  console.log(t)
+  // let n = await docker.listNodes()
+  // let s = docker.listServices()
+  // let t = docker.listTasks()
+  // console.log('Nodes: ', n.length)
+  // let n0 = docker.getNode(n[0].ID)
+  // let i = await n0.inspect()
+  // let upd = {
+  //   version: i.Version.Index,
+  //   ...i.Spec,
+  //   Labels: { a: "AAA", b: "BBB" },
+  // }
+  // let z = await n0.update(upd)
+}
+
+async function buildTable() {
+  const label = getLabel()
+  let services = {}
+  for (const svc of await docker.listServices()) {
+    let L = svc.Spec.Labels[label]
+    if (L)
+      services[svc.ID] = L
   }
-  let z = await n0.update(upd)
+  let nodes = {}
+  for (const task of await docker.listTasks()) {
+    let L = services[task.ServiceID]
+    if (!L || !task.NodeID)
+      continue
+    (nodes[task.NodeID] ||= new Set()).add(L)
+  }
+  return {
+    label,
+    labels: new Set(Object.values(services)),
+    nodes,
+  }
+}
+
+function getLabel() {
+  return process.env.SWARM_LABEL || 'ukoloff.swarm.label'
 }
