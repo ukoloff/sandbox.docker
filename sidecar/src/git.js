dec56805c69fe38808988015b99ef57ed582f8fa
Stas Ukolov <ukoloff@gmail.com>
2025-08-28 11:12:52 +0500

    Start git clone

---
+++ sidecar/src/git.js
@@ -0,0 +1,4 @@
+export async function clone(url, cwd=process.cwd()) {
+  let U = new URL(url, 'https://github.com/')
+  console.log(cwd)
+}
---
+++ sidecar/test/git.js
@@ -0,0 +1,6 @@
+import { it, describe } from 'node:test'
+import { clone } from '../src/git.js'
+
+describe('git', $ => {
+
+})
