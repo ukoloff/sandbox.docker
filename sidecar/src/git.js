c240eab1e2407748ed08060f43c33481f327476b
Stas Ukolov <ukoloff@gmail.com>
2025-08-28 13:25:30 +0500

    Clone!

--- sidecar/src/git.js
+++ sidecar/src/git.js
@@ -1,4 +1,37 @@
-export async function clone(url, cwd=process.cwd()) {
+import { spawn } from 'node:child_process'
+import { resolve } from 'node:path'
+
+export async function clone(url, cwd = '') {
+  cwd = resolve(cwd)
   let U = new URL(url, 'https://github.com/')
-  console.log(cwd)
+  let [base, folder] = splitPath(U.pathname)
+  U.pathname = base
+  let branch = U.hash.replace(/^#?/, '')
+  U.hash = ''
+  let repo = U.toString()
+  let args = 'clone -n --depth=1 --filter=tree:0'.split(' ')
+  if (branch)
+    args.push('--branch', branch)
+  args.push(U.toString(), '.')
+  let options = {cwd: cwd, stdio: 'inherit'}
+  let res = await wait(spawn('git', args, options))
+  if (folder) {
+    let res = await wait(spawn('git',
+      ['sparse-checkout', 'set', '--no-cone', `/${folder}`],
+      options))
+  }
+  res = await wait(spawn('git', ['checkout'], options))
+}
+
+function splitPath(path) {
+  let folders = path.split('/').filter($ => $)
+  return [folders.slice(0, 2).join('/'), folders.slice(2).join('/')]
+}
+
+function wait(child) {
+  return new Promise(function (resolve, reject) {
+    child
+      .on('error', reject)
+      .on('exit', resolve)
+  })
 }
