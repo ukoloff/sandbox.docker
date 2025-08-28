a838c0fe3b9751d58d97e179f2a4298bcc29d902
Stas Ukolov <ukoloff@gmail.com>
2025-08-28 13:08:20 +0500

    Use temp folder(s)

--- sidecar/test/git.js
+++ sidecar/test/git.js
@@ -1,6 +1,27 @@
-import { it, describe } from 'node:test'
+import { it, describe, after } from 'node:test'
+import { tmpdir } from 'node:os'
+import { join } from 'node:path'
+import { mkdtemp, rm } from 'node:fs/promises'
 import { clone } from '../src/git.js'
 
 describe('git', $ => {
+  let tmps = []
+  after(cleanUp)
 
+  it('clones repo', async $ => {
+    await clone('ukoloff/sandbox.docker/autolabel#al.js', await tmp())
+  })
+
+  async function tmp() {
+    let tmp = await mkdtemp(join(tmpdir(), 'sidecar-git-'))
+    tmps.push(tmp)
+    return tmp
+  }
+
+  async function cleanUp() {
+    for (let tmp of tmps) {
+      rm(tmp, { recursive: true })
+        .catch($ => 0)
+    }
+  }
 })
