17674f0bc1a910416ba6c3fc0fab91279deead73
Stas Ukolov <ukoloff@gmail.com>
2025-10-08 14:09:15 +0500

    Revert "Try ed25519 certificates -- Not accepted"
    This reverts commit 142ba0d3d76ca1e93872b874cdfbd219006b5d81.

--- sstp/scripts/ca.sh
+++ sstp/scripts/ca.sh
@@ -5,7 +5,7 @@ mkdir -p ca
 
 if [ ! -e ca/ca.key ]
 then
-  openssl genpkey -algorithm ED25519 -out ca/ca.key
+  openssl genrsa -out ca/ca.key 4096
 fi
 
 if [ ! -e ca/ca.cer ]
@@ -14,6 +14,6 @@ then
   openssl x509 -in ca/ca.cer -noout -text >> ca/ca.cer
 fi
 
-openssl genpkey -algorithm ED25519 -out sstp.key
+openssl genrsa -out sstp.key 2048
 openssl req -key sstp.key -x509 -days 90 -CA ca/ca.cer -CAkey ca/ca.key -addext basicConstraints=CA:FALSE -subj /CN=${X509CN:-SSTP} -out sstp.cer
 openssl x509 -in sstp.cer -noout -text >> sstp.cer
