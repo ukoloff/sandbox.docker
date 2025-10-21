553f5b1367f67ea06bb9b557bba8f5248e9f6634
Stas Ukolov <ukoloff@gmail.com>
2025-10-21 06:43:40 +0000

    Extract git URL parse

--- sidecar/src/git.js
+++ sidecar/src/git.js
@@ -1,36 +1,42 @@
 import { resolve } from 'node:path'
 import { wait, run, spawn } from './run.js'
 
+export function parse(uri) {
+  let u = new URL(uri, 'https://github.com/')
+  let params = Object.fromEntries(u.searchParams)
+  u.search = ''
+  let folders = u.pathname.split('/').filter($ => $)
+  let slug = folders.slice(0, 2).join('/')
+  let folder = folders.slice(2).join('/')
+  u.pathname = slug
+  let ref = u.hash.replace(/^#?/, '')
+  u.hash = ''
+  let repo = u.toString()
+  u.username = ''
+  u.password = ''
+  let safe = u.toString()
+  return { src: uri, repo, safe, slug, folder, ref, params }
+}
+
 export async function isGit(cwd = '') {
-  cwd = resolve(cwd)
-  let git = spawn('git', ['rev-parse', '--is-inside-work-tree'], { cwd: cwd })
+  let git = spawn('git', ['rev-parse', '--is-inside-work-tree'], { cwd })
   let res = await wait(git)
   return !res
 }
 
 export async function clone(url, cwd = '') {
-  cwd = resolve(cwd)
-  let U = new URL(url, 'https://github.com/')
-  let [base, folder] = splitPath(U.pathname)
-  U.pathname = base
-  let branch = U.hash.replace(/^#?/, '')
-  U.hash = ''
-  let repo = U.toString()
+  let src = 'string' == typeof (url) ? parse(url) : url
   let args = 'clone -q -n --depth=1 --filter=tree:0'.split(' ')
-  if (branch)
-    args.push('--branch', branch)
-  args.push(U.toString(), '.')
-  let options = { cwd: cwd, stdio: 'inherit' }
+  if (src.ref)
+    args.push('--branch', src.ref)
+  args.push(src.repo, '.')
+  let options = { cwd, stdio: 'inherit' }
   await run('git', args, options)
-  if (folder) {
+  if (src.folder) {
     await run('git',
-      ['sparse-checkout', 'set', '--no-cone', `/${folder}/`],
+      ['sparse-checkout', 'set', '--no-cone', `/${src.folder}/`],
       options)
   }
   await run('git', ['checkout', '-q'], options)
 }
 
-function splitPath(path) {
-  let folders = path.split('/').filter($ => $)
-  return [folders.slice(0, 2).join('/'), folders.slice(2).join('/')]
-}
