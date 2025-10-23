1c05ad5ce831691bfab8e9423ce5405a183f5a58
Stas Ukolov <ukoloff@gmail.com>
2025-10-23 08:15:17 +0500

    Format

--- sidecar/src/run.js
+++ sidecar/src/run.js
@@ -2,15 +2,15 @@ import { spawn } from 'node:child_process'
 export { spawn } from 'node:child_process'
 
 export function wait(child) {
-    return new Promise(function (resolve, reject) {
-        child
-            .on('error', reject)
-            .on('exit', resolve)
-    })
+  return new Promise(function (resolve, reject) {
+    child
+      .on('error', reject)
+      .on('exit', resolve)
+  })
 }
 
 export async function run(...params) {
-    let res = await wait(spawn(...params))
-    if (res)
-        throw Error(`Exited with code #${res}`)
+  let res = await wait(spawn(...params))
+  if (res)
+    throw Error(`Exited with code #${res}`)
 }
