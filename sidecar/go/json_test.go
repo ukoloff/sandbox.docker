2772aac4379d15c47dedfbba968c101693699768
Stas Ukolov <ukoloff@gmail.com>
2025-10-30 22:40:14 +0500

    Add more tests

--- sidecar/go/json_test.go
+++ sidecar/go/json_test.go
@@ -6,17 +6,42 @@ import (
 )
 
 func TestStringify(t *testing.T) {
+	pi := 3.14
+	five := 5
 	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
+		name    string // description of this test case
 		data    any
 		want    string
 		wantErr bool
-	}{
-		{name: "Empty",
-			data: struct{ A string }{A: "Hello, world!"},
-			want: "{\"A\":\"Hello, world!\"}"},
+	}{{name: "Struct",
+		data: struct{ A string }{A: "Hello, world!"},
+		want: "{\"A\":\"Hello, world!\"}"},
+		{name: "Array",
+			data: []int{3, 14, 159, 2, 6, 535},
+			want: "[3,14,159,2,6,535]"},
+		{name: "Number",
+			data: 3.14,
+			want: "3.14"},
+		{name: "Number PTR",
+			data: &pi,
+			want: "3.14"},
+		{name: "NULL",
+			data: nil,
+			want: "null"},
+		{name: "EmptyPTR",
+			data: struct {
+				M *int
+				N *int `json:",omitempty"`
+			}{M: &five},
+			want: "{\"M\":5}"},
+		{name: "Omit0",
+			data: struct {
+				M int
+				N int `json:",omitzero"`
+			}{M: 6 * 7},
+			want: "{\"M\":42}"},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, gotErr := main.Stringify(tt.data)
@@ -29,7 +54,6 @@ func TestStringify(t *testing.T) {
 			if tt.wantErr {
 				t.Fatal("Stringify() succeeded unexpectedly")
 			}
-
 			if got != tt.want {
 				t.Errorf("Stringify() = %v, want %v", got, tt.want)
 			}
