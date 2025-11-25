9234ccac866324ba64982f6fef397ef4a9e8c17b
Stas Ukolov <ukoloff@gmail.com>
2025-11-25 18:13:36 +0000

    Run simple tests for sh

--- sidecar/test/sh.js
+++ sidecar/test/sh.js
@@ -1,13 +1,17 @@
 import { it, describe, after } from 'node:test'
-import { isLinux } from '../src/sh.js'
+import { isLinux, sh } from '../src/sh.js'
+import { run } from '../src/run.js'
+import { join } from 'path'
 
 const skip = !await isLinux()
 
 describe('Shell', { skip }, $ => {
-  it('runs', $ => {
+  it('runs', async $ => {
+    await sh(join(import.meta.dirname, 'sh/true.sh'))
   })
 
-  it ('fails', $ => {
-
+  it('fails', async $ => {
+    await $.assert.rejects(async $ =>
+      await sh(join(import.meta.dirname, 'sh/false.sh')))
   })
 })
