28e456650d32a2e1a69cd0e912ae1400f61d495e
Stas Ukolov <ukoloff@gmail.com>
2025-11-25 18:39:44 +0500

    Start sh module

---
+++ sidecar/src/sh.js
@@ -0,0 +1,16 @@
+import { access, constants } from 'node:fs/promises'
+import { run } from './run'
+import { chdir } from 'node:process'
+
+const SHELL = '/bin/sh'
+
+export async function isLinux() {
+  try {
+    await access(SHELL, constants.F_OK)
+    return true
+  } catch { }
+}
+
+export async function sh(file, folder = '') {
+  return run(SHELL, ['-e', file], {chdir: folder})
+}
