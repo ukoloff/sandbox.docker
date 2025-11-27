833dd057c55cb070318eb20c60749f8711d10262
Stas Ukolov <ukoloff@gmail.com>
2025-11-27 06:42:16 +0000

    Test SIDECAR_GIT_URL var

--- sidecar/test/spawn.js
+++ sidecar/test/spawn.js
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { tmpdir } from 'node:os'
 import { it, before, after } from 'node:test'
-import { rm, mkdtemp, access, readFile, constants } from 'node:fs/promises'
+import { rm, mkdtemp, access, readFile, constants, mkdir } from 'node:fs/promises'
 import { run } from '../src/run.js'
 import { isLinux } from '../src/sh.js'
 
@@ -34,7 +34,21 @@ it('Exec', async $ => {
       $.assert.equal(b.toString().trim(), 'Non-B!')
       let z = await readFile(join(home, 'z.txt'))
       $.assert.equal(z.toString().trim(), 'Z!')
+
+      it('envvar', async $ => {
+        let repo = join(tmp, 'dev/clone.env')
+        await mkdir(repo, {recursive:true})
+        process.env.SIDECAR_GIT_URL = 'ukoloff/sandbox.docker/sidecar/test/sh.d#sidecar'
+        await run('node', [join(import.meta.dirname, '..')], { cwd: repo })
+        delete process.env.SIDECAR_GIT_URL
+        let home = join(repo, 'sidecar/test/sh.d')
+        let a = await readFile(join(home, 'a.txt'))
+        $.assert.equal(a.toString().trim(), 'A!')
+        let b = await readFile(join(home, 'b.txt'))
+        $.assert.equal(b.toString().trim(), 'Non-B!')
+        let z = await readFile(join(home, 'z.txt'))
+        $.assert.equal(z.toString().trim(), 'Z!')
+      })
     })
   })
-
 })
