be3a1747377725922cade19c4e1a267ebe04c6d4
Stas Ukolov <ukoloff@gmail.com>
2025-12-02 10:42:20 +0500

    Create SSH keys

--- sync/Rclone/config/.sidecar/ssh-keys.sh
+++ sync/Rclone/config/.sidecar/ssh-keys.sh
@@ -1,2 +1,16 @@
+apk add --no-cache openssh-keygen
+
 mkdir -p sftp/.ssh
 mkdir -p mnt22/.ssh
+
+if [ ! -f sftp/.ssh/id_ed25519 ]
+then
+  ssh-keygen -t ed25519 -f sftp/.ssh/id_ed25519 -N ''
+  echo sftp $(cat sftp/.ssh/id_ed25519.pub) > mnt22/.ssh/known_hosts
+fi
+
+if [ ! -f mnt22/.ssh/id_ed25519 ]
+then
+  ssh-keygen -t ed25519 -f mnt22/.ssh/id_ed25519 -N ''
+  cat mnt22/.ssh/id_ed25519.pub > sftp/.ssh/authorized_keys
+fi
