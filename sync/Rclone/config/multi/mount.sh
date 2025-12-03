b74cf2fe406b09c69007663fbf59b170e2def078
Stas Ukolov <ukoloff@gmail.com>
2025-12-03 11:43:49 +0500

    --dir-cache-time 1s

--- sync/Rclone/config/multi/mount.sh
+++ sync/Rclone/config/multi/mount.sh
@@ -3,5 +3,5 @@
 for remote in http webdav sftp
 do
   mkdir -p /mnt/$remote
-  rclone rc mount/mount fs=$remote: mountPoint=/mnt/$remote mountOpt='{"AllowNonEmpty": true}'
+  rclone rc mount/mount fs=$remote: mountPoint=/mnt/$remote mountOpt='{"AllowNonEmpty": true}' vfsOpt='{"DirCacheTime": 1000000000}'
 done
