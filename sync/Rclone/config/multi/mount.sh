8f6b5e4a560dc9ed74667864b5db0dd153be9263
Stas Ukolov <ukoloff@gmail.com>
2025-12-03 12:00:55 +0500

    Suppress output

--- sync/Rclone/config/multi/mount.sh
+++ sync/Rclone/config/multi/mount.sh
@@ -6,8 +6,8 @@
   for remote in http webdav sftp
   do
     mkdir -p /mnt/$remote
-    rclone rc mount/mount fs=$remote: mountPoint=/mnt/$remote mountOpt='{"AllowNonEmpty": true}' vfsOpt='{"DirCacheTime": 1000000000}'
+    rclone rc --no-output mount/mount fs=$remote: mountPoint=/mnt/$remote mountOpt='{"AllowNonEmpty": true}' vfsOpt='{"DirCacheTime": 1000000000}'
   done
-) >/dev/null &
+) &
 
 exec rclone rcd --rc-no-auth --rc-addr :5572 --rc-web-gui --rc-web-gui-no-open-browser
