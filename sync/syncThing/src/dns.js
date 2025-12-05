fca2b7da7c9fcb3a124a97f026501cbae8d9bf0b
Stas Ukolov <ukoloff@gmail.com>
2025-12-05 09:30:49 +0500

    Rename nodes

--- sync/syncThing/src/dns.js
+++ sync/syncThing/src/dns.js
@@ -1,7 +1,16 @@
 import { resolve } from 'node:dns/promises'
 
-export async function nodes(name = 'sthng') {
-    let ns = await resolve(name)
+export async function nodes(name = 'st-node') {
+    let ns = await R(name)
+    let tns = await R(`tasks.${name}`)
     ns.sort()
     return ns
+}
+
+async function R(name) {
+  try {
+    return await resolve(name)
+  } catch {
+    return []
+  }
 }
